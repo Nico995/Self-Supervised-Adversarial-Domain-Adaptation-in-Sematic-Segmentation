7393d9e105f2f65cfe641b0a5785840fe03a1aa3
Nicola <occelli.nicola@gmail.com>
2021-05-09 12:06:55 +0200

    docstrings and code refactoring

--- model/build_BiSeNet.py
+++ model/build_BiSeNet.py
@@ -1,93 +1,172 @@
 import torch
 from torch import nn
+from torch.nn.functional import interpolate
+
 from model.build_contextpath import build_contextpath
-import warnings
-warnings.filterwarnings(action='ignore')
+
 
 class ConvBlock(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=2,padding=1):
+    """
+    Standard [Conv + Batch Norm + ReLu] block implementation.
+
+    Args:
+        in_channels (int): Number of channels in inputs image.
+        out_channels (int): Number of output feature maps.
+        kernel_size (int, optional): Size of the convolving kernel. Default: 3.
+        stride (int, optional): Stride of the convolution. Default: 2.
+        padding (int, optional): Zero-padding added to both sides of the inputs. Default: 1.
+
+    Attributes:
+        conv1 (nn.Conv2d): Convolutional layer of the block
+        bn (nn.BatchNorm2d): Batch normalization layer of the block
+        relu (nn.ReLu): Activation layer of the block
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=2, padding=1):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
+                               bias=False)
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
 
-    def forward(self, input):
-        x = self.conv1(input)
+    def forward(self, inputs):
+        x = self.conv1(inputs)
         return self.relu(self.bn(x))
 
+
 class Spatial_path(torch.nn.Module):
+    # TODO: Parametrize in/out channels of the conv-blocks
+    """
+    Spatial Path model branch. This path is supposed to encode "rich spatial information".
+    It is made up of 3 convolutional blocks, which in total will resize the inputs image to 1/8 of it's original size.
+
+    Args:
+
+    Attributes:
+        conv_block1 (ConvBlock): First convolutional block of the Spatial Path
+        conv_block2 (ConvBlock): Second convolutional block of the Spatial Path
+        conv_block3 (ConvBlock): Third (and last) convolutional block of the Spatial Path
+    """
+
     def __init__(self):
         super().__init__()
-        self.convblock1 = ConvBlock(in_channels=3, out_channels=64)
-        self.convblock2 = ConvBlock(in_channels=64, out_channels=128)
-        self.convblock3 = ConvBlock(in_channels=128, out_channels=256)
+        self.conv_block1 = ConvBlock(in_channels=3, out_channels=64)
+        self.conv_block2 = ConvBlock(in_channels=64, out_channels=128)
+        self.conv_block3 = ConvBlock(in_channels=128, out_channels=256)
 
-    def forward(self, input):
-        x = self.convblock1(input)
-        x = self.convblock2(x)
-        x = self.convblock3(x)
+    def forward(self, inputs):
+        x = self.conv_block1(inputs)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
         return x
 
+
 class AttentionRefinementModule(torch.nn.Module):
+    """
+    Custom module to refine the features from the Context Path.
+    It employs Global Average Pooling to capture the Global context, and is applied at certain layers of the context
+    path.
+    It outputs an attention vector to guide the feature learning.
+
+    Args:
+        in_channels (int): Number of input feature maps.
+        out_channels (int): Number of output feature maps.
+
+    Attributes:
+        in_channels (int): Number of input feature maps.
+        avg_pool (nn.AdaptiveAvgPool2d): Global Average Pooling layer.
+        conv (nn.Conv2d): 1x1 Convolutional layer.
+        bn (nn.BatchNorm2d): Batch normalization layer.
+        sigmoid (nn.Sigmoid): Sigmoid Activation layer.
+    """
+
     def __init__(self, in_channels, out_channels):
         super().__init__()
+        self.in_channels = in_channels
+        self.avg_pool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
         self.bn = nn.BatchNorm2d(out_channels)
         self.sigmoid = nn.Sigmoid()
-        self.in_channels = in_channels
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
 
-    def forward(self, input):
+    def forward(self, inputs):
         # global average pooling
-        x = self.avgpool(input)
+        x = self.avg_pool(inputs)
+
+        # input and x channels should be same
         assert self.in_channels == x.size(1), 'in_channels and out_channels should all be {}'.format(x.size(1))
+
         x = self.conv(x)
-        # x = self.sigmoid(self.bn(x))
         x = self.sigmoid(x)
-        # channels of input and x should be same
-        x = torch.mul(input, x)
+
+        x = torch.mul(inputs, x)
         return x
 
 
 class FeatureFusionModule(torch.nn.Module):
+    """
+    Custom module to allow feature fusion coming from Context Path and Spatial Path.
+    Since the features coming from these 2 modules have different representation (come from 2 different "models"), we
+    need a way to combine them. This module is the way described in the paper.
+
+    Args:
+        num_classes (int): Number of target classes for the task at hand.
+        in_channels (int): Number of total input channels, coming from both Spatial and Context Path.
+
+    Attributes:
+        in_channels (int): Number of total input channels, coming from both Spatial and Context Path.
+        avg_pool (nn.AdaptiveAvgPool2d): Global Average Pooling layer.
+        conv_block (ConvBlock): Convolutional block (convolutional + batch norm + ReLu).
+        conv1 (nn.Conv2d): First 1x1 Convolutional Layer.
+        relu (nn.ReLU): First Activation layer (ReLu).
+        conv2 (nn.Conv2d): Second 1x1 Convolutional Layer.
+        sigmoid (nn.ReLU): Second Activation layer (Sigmoid).
+    """
+
     def __init__(self, num_classes, in_channels):
         super().__init__()
-        # self.in_channels = input_1.channels + input_2.channels
-        # resnet101 3328 = 256(from context path) + 1024(from spatial path) + 2048(from spatial path)
-        # resnet18  1024 = 256(from context path) + 256(from spatial path) + 512(from spatial path)
         self.in_channels = in_channels
 
-        self.convblock = ConvBlock(in_channels=self.in_channels, out_channels=num_classes, stride=1)
+        self.avg_pool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.conv_block = ConvBlock(in_channels=self.in_channels, out_channels=num_classes, stride=1)
         self.conv1 = nn.Conv2d(num_classes, num_classes, kernel_size=1)
         self.relu = nn.ReLU()
         self.conv2 = nn.Conv2d(num_classes, num_classes, kernel_size=1)
         self.sigmoid = nn.Sigmoid()
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
 
+    def forward(self, inputs_1, inputs_2):
+        x = torch.cat((inputs_1, inputs_2), dim=1)
 
-    def forward(self, input_1, input_2):
-        x = torch.cat((input_1, input_2), dim=1)
+        # input and x channels should be same
         assert self.in_channels == x.size(1), 'in_channels of ConvBlock should be {}'.format(x.size(1))
-        feature = self.convblock(x)
-        x = self.avgpool(feature)
+        feature = self.conv_block(x)
 
+        x = self.avg_pool(feature)
         x = self.relu(self.conv1(x))
         x = self.sigmoid(self.conv2(x))
         x = torch.mul(feature, x)
         x = torch.add(x, feature)
         return x
 
+
 class BiSeNet(torch.nn.Module):
-    def __init__(self, num_classes, context_path):
+    """
+    BiSeNet implementation.
+
+    Args:
+        num_classes (int): Number of target classes for the task at hand.
+        backbone (str): Name of the model to use for the backbone of the Context Path.
+    """
+
+    def __init__(self, num_classes, backbone):
         super().__init__()
         # build spatial path
         self.saptial_path = Spatial_path()
 
         # build context path
-        self.context_path = build_contextpath(name=context_path)
+        self.context_path = build_contextpath(name=backbone)
 
         # build attention refinement module  for resnet 101
-        if context_path == 'resnet101':
+        if backbone == 'resnet101':
             self.attention_refinement_module1 = AttentionRefinementModule(1024, 1024)
             self.attention_refinement_module2 = AttentionRefinementModule(2048, 2048)
             # supervision block
@@ -96,7 +175,8 @@ class BiSeNet(torch.nn.Module):
             # build feature fusion module
             self.feature_fusion_module = FeatureFusionModule(num_classes, 3328)
 
-        elif context_path == 'resnet18':
+        # build attention refinement module  for resnet 18
+        elif backbone == 'resnet18':
             # build attention refinement module  for resnet 18
             self.attention_refinement_module1 = AttentionRefinementModule(256, 256)
             self.attention_refinement_module2 = AttentionRefinementModule(512, 512)
@@ -133,25 +213,24 @@ class BiSeNet(torch.nn.Module):
                     nn.init.constant_(m.weight, 1)
                     nn.init.constant_(m.bias, 0)
 
-    def forward(self, input):
-        # output of spatial path
-        sx = self.saptial_path(input)
+    def forward(self, inputs):
 
+        # output of spatial path
+        sx = self.saptial_path(inputs)
         # output of context path
-        cx1, cx2, tail = self.context_path(input)
+        cx1, cx2, tail = self.context_path(inputs)
+
+        # ARM 1
         cx1 = self.attention_refinement_module1(cx1)
+        # ARM 2
         cx2 = self.attention_refinement_module2(cx2)
+
         cx2 = torch.mul(cx2, tail)
+
         # upsampling
         cx1 = torch.nn.functional.interpolate(cx1, size=sx.size()[-2:], mode='bilinear')
         cx2 = torch.nn.functional.interpolate(cx2, size=sx.size()[-2:], mode='bilinear')
         cx = torch.cat((cx1, cx2), dim=1)
-
-        if self.training == True:
-            cx1_sup = self.supervision1(cx1)
-            cx2_sup = self.supervision2(cx2)
-            cx1_sup = torch.nn.functional.interpolate(cx1_sup, size=input.size()[-2:], mode='bilinear')
-            cx2_sup = torch.nn.functional.interpolate(cx2_sup, size=input.size()[-2:], mode='bilinear')
 
         # output of feature fusion module
         result = self.feature_fusion_module(sx, cx)
@@ -160,28 +239,14 @@ class BiSeNet(torch.nn.Module):
         result = torch.nn.functional.interpolate(result, scale_factor=8, mode='bilinear')
         result = self.conv(result)
 
-        if self.training == True:
+        if self.training:
+            # ??? Page 7 "loss function"
+            cx1_sup = self.supervision1(cx1)
+            cx2_sup = self.supervision2(cx2)
+            # ???
+            cx1_sup = torch.nn.functional.interpolate(cx1_sup, size=inputs.size()[-2:], mode='bilinear')
+            cx2_sup = torch.nn.functional.interpolate(cx2_sup, size=inputs.size()[-2:], mode='bilinear')
+
             return result, cx1_sup, cx2_sup
-
-        return result
-
-
-if __name__ == '__main__':
-    import os
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-    model = BiSeNet(32, 'resnet18')
-    # model = nn.DataParallel(model)
-
-    model = model.cuda()
-    x = torch.rand(2, 3, 256, 256)
-    record = model.parameters()
-    # for key, params in model.named_parameters():
-    #     if 'bn' in key:
-    #         params.requires_grad = False
-    from utils import group_weight
-    # params_list = []
-    # for module in model.mul_lr:
-    #     params_list = group_weight(params_list, module, nn.BatchNorm2d, 10)
-    # params_list = group_weight(params_list, model.context_path, torch.nn.BatchNorm2d, 1)
-
-    print(model.parameters())
+        else:
+            return result
