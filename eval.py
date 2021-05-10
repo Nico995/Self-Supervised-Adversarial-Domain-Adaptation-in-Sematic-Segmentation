0d0aa643c5afee36522c485cb67b73b708691c9e
Nicola <occelli.nicola@gmail.com>
2021-05-10 09:40:34 +0200

    refactoring and cleaning of dead code

--- eval.py
+++ eval.py
@@ -3,102 +3,75 @@ import torch
 import argparse
 import os
 from torch.utils.data import DataLoader
-from model import BiSeNet
+from model.build_BiSeNet import BiSeNet
 import numpy as np
-from utils import reverse_one_hot, compute_global_accuracy, fast_hist, per_class_iu, cal_miou
+from utils import reverse_one_hot, global_accuracy, get_confusion_matrix, intersection_over_union
 import tqdm
 
 
-def eval(model,dataloader, args, csv_path):
-    print('start test!')
+def validate(args, model, dataloader, csv_path):
+    """
+    Perform a validation epoch.
+
+    Args:
+        args (Namespace): Command line arguments.
+        model: Model to evaluate.
+        dataloader (DataLoader): Validation data loader.
+        csv_path (str): Path to csv metadata
+
+    Returns:
+        Metrics
+    """
+
+    # Don't compute gradients during evaluation step
     with torch.no_grad():
+        # Disable dropout and batch norm layers
         model.eval()
-        precision_record = []
+
+        # Progress bar
         tq = tqdm.tqdm(total=len(dataloader) * args.batch_size)
         tq.set_description('test')
-        hist = np.zeros((args.num_classes, args.num_classes))
+
+        precision_record = []
+
+        confusion_matrix = np.zeros((args.num_classes, args.num_classes))
+        # Batch loop
         for i, (data, label) in enumerate(dataloader):
-            tq.update(args.batch_size)
+
+            # Move images to gpu
             if torch.cuda.is_available() and args.use_gpu:
                 data = data.cuda()
                 label = label.cuda()
+
+            # get model output
             predict = model(data).squeeze()
             predict = reverse_one_hot(predict)
-            predict = np.array(predict)
-            # predict = colour_code_segmentation(np.array(predict), label_info)
+            predict = np.array(predict.detach().cpu())
 
+            # get RGB label image
             label = label.squeeze()
             if args.loss == 'dice':
                 label = reverse_one_hot(label)
-            label = np.array(label)
+            label = np.array(label.detach().cpu())
+
+            # compute per pixel accuracy
+            precision = global_accuracy(predict, label)
+            confusion_matrix += get_confusion_matrix(label.flatten(), predict.flatten(), args.num_classes)
+
+            # there is no need to transform the one-hot array to visual RGB array
+            # predict = colour_code_segmentation(np.array(predict), label_info)
             # label = colour_code_segmentation(np.array(label), label_info)
-
-            precision = compute_global_accuracy(predict, label)
-            hist += fast_hist(label.flatten(), predict.flatten(), args.num_classes)
             precision_record.append(precision)
+
+            # Progress bar
+            tq.update(args.batch_size)
+
         precision = np.mean(precision_record)
-        miou_list = per_class_iu(hist)[:-1]
-        miou_dict, miou = cal_miou(miou_list, csv_path)
-        print('IoU for each class:')
-        for key in miou_dict:
-            print('{}:{},'.format(key, miou_dict[key]))
+        per_class_iou, mean_iou = intersection_over_union(confusion_matrix, csv_path)
+
+        print('Global Precision [eval]: %.3f' % precision)
+        print('Mean IoU [eval]: %.3f' % mean_iou)
+        print('Per Class IoU [eval]: \n', list(per_class_iou))
         tq.close()
-        print('precision for test: %.3f' % precision)
-        print('mIoU for validation: %.3f' % miou)
-        return precision
 
-def main(params):
-    # basic parameters
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--checkpoint_path', type=str, default=None, required=True, help='The path to the pretrained weights of model')
-    parser.add_argument('--crop_height', type=int, default=720, help='Height of cropped/resized input image to network')
-    parser.add_argument('--crop_width', type=int, default=960, help='Width of cropped/resized input image to network')
-    parser.add_argument('--data', type=str, default='/path/to/data', help='Path of training data')
-    parser.add_argument('--batch_size', type=int, default=1, help='Number of images in each batch')
-    parser.add_argument('--context_path', type=str, default="resnet101", help='The context path model you are using.')
-    parser.add_argument('--cuda', type=str, default='0', help='GPU ids used for training')
-    parser.add_argument('--use_gpu', type=bool, default=True, help='Whether to user gpu for training')
-    parser.add_argument('--num_classes', type=int, default=32, help='num of object classes (with void)')
-    parser.add_argument('--loss', type=str, default='dice', help='loss function, dice or crossentropy')
-    args = parser.parse_args(params)
-
-    # create dataset and dataloader
-    test_path = os.path.join(args.data, 'test')
-    # test_path = os.path.join(args.data, 'train')
-    test_label_path = os.path.join(args.data, 'test_labels')
-    # test_label_path = os.path.join(args.data, 'train_labels')
-    csv_path = os.path.join(args.data, 'class_dict.csv')
-    dataset = CamVid(test_path, test_label_path, csv_path, image_size=(args.crop_height, args.crop_width), mode='test')
-    dataloader = DataLoader(
-        dataset,
-        batch_size=1,
-        shuffle=True,
-        num_workers=4,
-    )
-
-    # build model
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.cuda
-    model = BiSeNet(args.num_classes, args.context_path)
-    if torch.cuda.is_available() and args.use_gpu:
-        model = torch.nn.DataParallel(model).cuda()
-
-    # load pretrained model if exists
-    print('load model from %s ...' % args.checkpoint_path)
-    model.module.load_state_dict(torch.load(args.checkpoint_path))
-    print('Done!')
-
-    # get label info
-    # label_info = get_label_info(csv_path)
-    # test
-    eval(model, dataloader, args, csv_path)
-
-
-if __name__ == '__main__':
-    params = [
-        '--checkpoint_path', 'path/to/ckpt',
-        '--data', '/path/to/CamVid',
-        '--cuda', '0',
-        '--context_path', 'resnet18',
-        '--num_classes', '12'
-    ]
-    main(params)
+        return precision, mean_iou
