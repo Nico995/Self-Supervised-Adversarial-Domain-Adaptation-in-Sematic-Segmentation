5f724000d2c6e08b502aa057b0aeb1f4e4670428
Nicola <occelli.nicola@gmail.com>
2021-05-25 11:08:26 +0200

    example script to launch BiSeNet training

---
+++ scripts/example_camvid.sh
@@ -0,0 +1,3 @@
+python train.py --num_epochs 50 --batch_size 2 --learning_rate 0.025 --optimizer sgd --loss dice --checkpoint_step 20 \
+ --validation_step 10 --crop_height 720 --crop_width 960 --context_path resnet18 --num_classes 12 --data ./data/CamVid/ \
+ --save_model_path ./checkpoints --num_workers 6 --use_gpu --pre_encoded
